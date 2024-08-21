d82121347f786e99c83bf1febb0e0c3cdbde6386
booya-tech <booya.pannachai@gmail.com>
2024-08-21 16:16:48 +0700

    Initial Commit

---
+++ NetworkingAsyncAwait/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkingAsyncAwait
+//
+//  Created by Panachai Sulsaksakul on 8/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkingAsyncAwait/NetworkingAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingAsyncAwaitApp.swift
+//  NetworkingAsyncAwait
+//
+//  Created by Panachai Sulsaksakul on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
